7b84c33942ced246c735920a7c496722e273b11b
metwas <metwas.seup@gmail.com>
2026-01-30 09:22:20 +0000

    License changes + cleanup

--- src/bundlers/tsc.mjs
+++ src/bundlers/tsc.mjs
@@ -1,77 +1,74 @@
 /**
-     MIT License
-
-     @Copyright (c) Metwas
-
-     Permission is hereby granted, free of charge, to any person obtaining a copy
-     of this software and associated documentation files (the "Software"), to deal
-     in the Software without restriction, including without limitation the rights
-     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-     copies of the Software, and to permit persons to whom the Software is
-     furnished to do so, subject to the following conditions:
-
-     The above Copyright notice and this permission notice shall be included in all
-     copies or substantial portions of the Software.
-
-     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-     AUTHORS OR Copyright HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-     SOFTWARE.
-*/
+ * Copyright (c) Metwas
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 
 /**_-_-_-_-_-_-_-_-_-_-_-_-_- Imports  _-_-_-_-_-_-_-_-_-_-_-_-_-*/
 
-import { exec } from 'node:child_process';
+import { exec } from "node:child_process";
 
 /**_-_-_-_-_-_-_-_-_-_-_-_-_-          _-_-_-_-_-_-_-_-_-_-_-_-_-*/
 
 /**
  * Typescript transpiler command-line
- * 
+ *
  * @public
- * @type {String} 
+ * @type {String}
  */
-const TSC_COMMAND = 'tsc';
+const TSC_COMMAND = "tsc";
 
 /**
  * Invokes the @see nestjs bundler. This works only for node-based applications.
- * 
+ *
  * @public
- * @param {String} app 
- * @param {Object} options 
+ * @param {String} app
+ * @param {Object} options
  * @returns {Promise<Boolean>}
  */
-export const build = async (app, options) =>
-{
-       try
-       {
-              const build = () =>
-              {
-                     return new Promise((resolve, _) =>
-                     {
-                            const process = exec(TSC_COMMAND, (error, stdout, stderr) =>
-                            {
-                                   const hasError = error ?? stderr;
-                                   // resolve stdout error message, otherwise return true if succesful 
-                                   resolve(hasError ? { error: stdout } : true);
-                            });
+export const build = async (app, options) => {
+       try {
+              const build = () => {
+                     return new Promise((resolve, _) => {
+                            const process = exec(
+                                   TSC_COMMAND,
+                                   (error, stdout, stderr) => {
+                                          const hasError = error ?? stderr;
+                                          // resolve stdout error message, otherwise return true if succesful
+                                          resolve(
+                                                 hasError
+                                                        ? {
+                                                                 error:
+                                                                        hasError +
+                                                                        "\n" +
+                                                                        stdout,
+                                                          }
+                                                        : true,
+                                          );
+                                   },
+                            );
 
-                            process.on("error", (error) =>
-                            {
+                            process.on("error", (error) => {
                                    resolve({ error: error });
                             });
                      });
               };
 
               return await build();
-       }
-       catch (error)
-       {
+       } catch (error) {
               return {
-                     error: error.message
+                     error: error.message,
               };
        }
 };
